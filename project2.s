11a8437ad657c871d381f94c31fb7a6b25e7b0b7
Jenaba Sow <jenaba@Jenabas-MacBook-Pro.local>
2018-11-16 20:10:43 -0500

    declare variable names used in program: inputIsLong, inputIsEmpty, inputFromUser, inputIsInvalid

--- project2.s
+++ project2.s
@@ -1,2 +1,8 @@
 #x = id number 02865424 so N = 27 + (02865424 % 10) N = 27 + 4 N = 31
 
+.data
+	
+	inputIsLong:    .asciiz "Input is too long."
+	inputIsEmpty:   .asciiz "Input is empty"
+	inputFromUser:  .space 500
+	inputIsInvalid: .asciiz "Invalid base-N number." #if N i soutside of the parameters 0-9 etc. 
